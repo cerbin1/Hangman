b7134d4c5ff91f23f3bc45ec5abe348971a06680
cerbin <bleble@d.com>
2016-10-21 20:42:00 +0200

    Added variables to operate on word to guess.

--- src/Words.java
+++ src/Words.java
@@ -1,7 +1,13 @@
 public class Words {
-    private String[] wordsToGuess = {"zubr", "kot",  "pies",  "szczur",  "hipopotam",  "chomik",  "zyrafa",  "slon",  "polacy",  "kon"};
+    private String[] wordsArray = {"zubr", "kot",  "pies",  "szczur",  "hipopotam",  "chomik",  "zyrafa",  "slon",  "polacy",  "kon"};
+    private String wordToGuess;
+    private int randomNumber;
+
+    public Words() {
+        randomNumber = (int)(Math.round(Math.random() * 10));
+    }
 
     public String getWordToGuess(int indexNumber) {
-        return wordsToGuess[indexNumber];
+        return wordsArray[indexNumber];
     }
 }
