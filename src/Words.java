f8768faf299ea935bd7a51f74e27424ceedc9e3a
cerbin <bleble@d.com>
2016-10-21 20:43:40 +0200

    Changed variable randomNumber to local.

--- src/Words.java
+++ src/Words.java
@@ -1,10 +1,9 @@
 public class Words {
     private String[] wordsArray = {"zubr", "kot",  "pies",  "szczur",  "hipopotam",  "chomik",  "zyrafa",  "slon",  "polacy",  "kon"};
     private String wordToGuess;
-    private int randomNumber;
 
     public Words() {
-        randomNumber = (int)(Math.round(Math.random() * 10));
+        int randomNumber = (int) (Math.round(Math.random() * 10));
     }
 
     public String getWordToGuess(int indexNumber) {
