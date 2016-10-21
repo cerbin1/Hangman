8f2d26d2b85f90a418eda0645297db08751c59ad
cerbin <bleble@d.com>
2016-10-21 20:33:51 +0200

    Added getWordToGuess();

--- src/Words.java
+++ src/Words.java
@@ -1,3 +1,7 @@
 public class Words {
     private String[] wordsToGuess = {"zubr", "kot",  "pies",  "szczur",  "hipopotam",  "chomik",  "zyrafa",  "slon",  "polacy",  "kon"};
+
+    public String getWordToGuess(int indexNumber) {
+        return wordsToGuess[indexNumber];
+    }
 }
